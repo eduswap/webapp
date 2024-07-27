13487dc98bccdc805914e7796907b6b20870223b
solidity12 <volibear1001@gmail.com>
2024-07-27 23:37:09 +0900

    feat: Add Footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <Topbar />
-
-  <router-view />
+  
+  <div class="router-container">
+    <router-view />
+  </div>
+  
+  <Footer />
 </template>
 
 <script setup>
 import Topbar from "./components/common/Topbar.vue";
+import Footer from "./components/common/Footer.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+.router-container {
+  min-height: calc(100vh - 163px);
+}
+</style>
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="footer-container">
+    <div class="footer-wrapper">
+      <div class="icon-wrapper">
+        <img src="@/assets/icon/x.svg" alt="x" />
+        <img src="@/assets/icon/medium.svg" alt="medium" />
+        <img src="@/assets/icon/gitbook.svg" alt="gitbook" />
+      </div>
+      <div class="footer-text">ⓒ EDUSWAP. LTD. All Rights Reserved.</div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.footer-container {
+  display: flex;
+  padding: 20px 80px;
+  justify-content: center;
+  align-items: flex-start;
+
+  background: #98cdc9;
+}
+
+.footer-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6px;
+}
+
+.icon-wrapper {
+  display: flex;
+  justify-content: space-between;
+  width: 113px;
+  height: 17px;
+}
+
+.icon-wrapper img {
+  cursor: pointer;
+}
+
+.footer-text {
+  color: #242a40;
+  font-family: Inter;
+  font-size: 13.125px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 19px;
+}
+</style>
