fffcc4b94ee50c7c53c3d5f60ec0bb4a6d95f836
solidity12 <volibear1001@gmail.com>
2024-07-29 16:07:26 +0900

    feat: Add Ing vue

---
+++ src/components/common/Ing.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="ing">
+      <img src="@/assets/ing.png" alt="" />
+    </div>
+  </template>
+  
+  <style>
+  .ing {
+    display: flex;
+    align-items: center;
+  }
+  
+  .ing img {
+    width: 70px;
+    height: 70px;
+    animation: spin 3s linear infinite;
+  }
+  
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  </style>
+  
