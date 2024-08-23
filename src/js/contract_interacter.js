4106cb95a3f172cf227a1cee3852f046e28ea1e8
solidity12 <volibear1001@gmail.com>
2024-08-24 06:53:52 +0900

    feat: Update js file

--- src/js/contract_interacter.js
+++ src/js/contract_interacter.js
@@ -1,5 +1,13 @@
 import { ethers } from "ethers";
 
+import {
+    createWeb3Modal,
+    defaultConfig,
+    useWeb3Modal,
+    useWalletInfo,
+    useWeb3ModalAccount,
+} from "@web3modal/ethers/vue";
+
 import usdcImg from "@/assets/token/usdc.png";
 import eduImg from "@/assets/token/edu.png";
 import ethImg from "@/assets/token/eth.png";
@@ -14,7 +22,7 @@ import paths from "@/constants/paths.json"
 let tokens = [
     {
         "symbol": "USDC",
-        "name": "eduswap test USDC",
+        "name": "test USDC",
         "address": "0x7aFB87aE9E37c365955012527f8a9039D6F2CA30",
         "img": usdcImg,
         "decimals": 6,
@@ -30,7 +38,7 @@ let tokens = [
     },
     {
         "symbol": "ETH",
-        "name": "eduswap test ETH",
+        "name": "test ETH",
         "address": "0x90f2F4E97Eb6B62D9049D07C6f6877FD171a9a0F",
         "img": ethImg,
         "decimals": 18,
@@ -38,7 +46,7 @@ let tokens = [
     },
     {
         "symbol": "ARB",
-        "name": "eduswap test ARB",
+        "name": "test ARB",
         "address": "0x3FBA3ef10e452D1e8Cc6C0cf552A8A25b572Ec41",
         "img": arbImg,
         "decimals": 18,
@@ -55,8 +63,6 @@ let tokens = [
 ]
 
 let provider = null;
-let signer = null;
-let account = null;
 
 let viewContract = null;
 let routerContract = null;
@@ -65,36 +71,77 @@ let tokenContract = {};
 const viewContractAddress = "0x8233B16904ceb6f34E3107464564a444daF4d3f2";
 const routerContractAddress = "0xe745f43775B760958cd34ee83B3ab0c088F74630";
 
+// 1. Get projectId from https://cloud.walletconnect.com
+const projectId = "ff3f8d2e88e862972b9d4c7c2b81acf0"; //TODO env
+
+// 2. Set chains
+const mainnet = {
+    chainId: 656476,
+    name: "Open Campus Codex Sepolia",
+    currency: "ETH",
+    explorerUrl: "https://opencampus-codex.blockscout.com",
+    rpcUrl: "https://rpc.open-campus-codex.gelato.digital",
+};
+
+// 3. Create your application's metadata object
+const metadata = {
+    name: "EDUSWAP",
+    description: "Whis is eduswap website",
+    url: "", //TODO 
+    icons: [""], //TODO 
+};
+
+// 4. Create Ethers config
+const ethersConfig = defaultConfig({
+    /*Required*/
+    metadata,
+
+    /*Optional*/
+    enableEIP6963: true, // true by default
+    enableInjected: true, // true by default
+    enableCoinbase: true, // true by default
+    rpcUrl: "https://rpc.open-campus-codex.gelato.digital", // used for the Coinbase SDK
+    defaultChainId: 656476, // used for the Coinbase SDK
+});
+
+// 5. Create a AppKit instance
+const modal = createWeb3Modal({
+    ethersConfig,
+    chains: [mainnet],
+    projectId,
+
+    /*Optional*/
+    enableAnalytics: true, // Optional - defaults to your Cloud configuration
+    enableSwaps: false,
+    enableOnramp: false,
+});
+
 function attach() {
-    if (provider == null) {
-        provider = new ethers.BrowserProvider(window.ethereum);
-    }
+    const walletProvider = modal.getWalletProvider();
 
-    if (viewContract == null) {
-        viewContract = new ethers.Contract(viewContractAddress, viewArtifacts.abi, provider);
-    }
+    if (walletProvider != undefined) {
+        if (provider == null) {
+            provider = new ethers.BrowserProvider(walletProvider);
+        }
 
-    if (routerContract == null) {
-        routerContract = new ethers.Contract(routerContractAddress, routerArtifacts.abi, provider);
-    }
+        if (viewContract == null) {
+            viewContract = new ethers.Contract(viewContractAddress, viewArtifacts.abi, provider);
+        }
 
-    if (Object.keys(tokenContract).length == 0) {
-        for (let i = 0; i < tokens.length; i++) {
-            tokenContract[tokens[i].address] = new ethers.Contract(tokens[i].address, tokenArtifacts.abi, provider);
+        if (routerContract == null) {
+            routerContract = new ethers.Contract(routerContractAddress, routerArtifacts.abi, provider);
+        }
+
+        if (Object.keys(tokenContract).length == 0) {
+            for (let i = 0; i < tokens.length; i++) {
+                tokenContract[tokens[i].address] = new ethers.Contract(tokens[i].address, tokenArtifacts.abi, provider);
+            }
         }
     }
 }
 
 function getTokens() {
     return tokens;
-}
-
-function getPaths() {
-    return paths;
-}
-
-function getAccount() {
-    return account;
 }
 
 function getImageSource(tokenAddress) {
@@ -108,11 +155,6 @@ async function updateTokenBalance(userAddress) {
     for (let i = 0; i < tokens.length; i++) {
         tokens[i].balance = ethers.formatUnits(balances[i], tokens[i].decimals);
     }
-}
-
-async function updateAccount() {
-    signer = await provider.getSigner();
-    account = await signer.getAddress();
 }
 
 async function getAmountsOutInfo(amountIn, tokenIn, tokenOut) {
@@ -161,7 +203,7 @@ async function getAmountsInInfo(amountOut, tokenIn, tokenOut) {
     return { amountIn, spotPrice, path }
 }
 
-async function checkAllowance(tokenAddress) {
+async function checkAllowance(tokenAddress, account) {
     if (account == null) {
         return true
     }
@@ -175,6 +217,8 @@ async function checkAllowance(tokenAddress) {
 }
 
 async function approveToken(tokenAddress) {
+    const signer = await provider.getSigner();
+    
     const tx = await tokenContract[tokenAddress]
         .connect(signer)
         .approve(
@@ -185,7 +229,9 @@ async function approveToken(tokenAddress) {
     console.log("approveToken:", tx.hash);
 }
 
-async function swapExactTokensForTokens(amountIn, amountOutMin, path) {
+async function swapExactTokensForTokens(amountIn, amountOutMin, path, account) {
+    const signer = await provider.getSigner();
+
     const fromDecimals = tokens.filter((token) => token.address == path[0])[0].decimals;
     const toDecimals = tokens.filter((token) => token.address == path[path.length - 1])[0].decimals;
 
@@ -241,13 +287,16 @@ async function swapExactTokensForTokens(amountIn, amountOutMin, path) {
 export {
     getTokens,
     attach,
-    getAccount,
     getImageSource,
     updateTokenBalance,
-    updateAccount,
     getAmountsOutInfo,
     getAmountsInInfo,
     checkAllowance,
     approveToken,
     swapExactTokensForTokens,
+
+    // wallet
+    useWeb3Modal,
+    useWalletInfo,
+    useWeb3ModalAccount,
 }
